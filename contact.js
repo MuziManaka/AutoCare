b8050897b988bf683392e8d30742a1a939330b47
Andile <andile7117@gmail.com>
2026-04-19 15:13:12 +0200

    Fix typos in contact.js and update Contact.html button type

---
+++ contact.js
@@ -0,0 +1,24 @@
+function sendmail() {
+    var params = {
+        name: document.getElementById("name").value,
+        phone: document.getElementById("phone").value,
+        email: document.getElementById("email").value,
+        car: document.getElementById("car").value,
+        message: document.getElementById("message").value,
+    };
+    const serviceId = "service_k798ncu";
+    const templateId = "template_8t8o6t6";
+
+    emailjs.send(serviceId, templateId, params).then(
+        (res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("phone").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("car").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message has been sent successfully!");
+        }
+    ).catch((err) => console.log(err));
+}
+
